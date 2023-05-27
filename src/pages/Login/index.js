e8c7f08b39aa53d5aa7827e08d0ec907ca5ddc80
Lul4t3ch <lul4t3ch15@gmail.com>
2023-05-27 13:41:33 -0300

    criando tela de login da aplicação

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from '../../styles/GlobalStyle';
+import { Form, Title } from './styled';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <Container>
-      <h1>Login</h1>
+      <Title>Login</Title>
+      <Form>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="your email"
+          />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="your password"
+          />
+        </label>
+        <button type="submit">Sign in</button>
+      </Form>
     </Container>
   );
 }
