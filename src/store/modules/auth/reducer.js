292a9c58a8013b794e1e7324ef506f5223a870e1
Lul4t3ch <lul4t3ch15@gmail.com>
2023-05-29 13:23:10 -0300

    configurando a parte de login da aplicação

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -10,9 +10,20 @@ const initialState = {
 // eslint-disable-next-line default-param-last
 export default (state = initialState, action) => {
   switch (action.type) {
-    case types.LOGIN_REQUEST: {
-      console.log('Reducer: ', action.payload);
-      return state;
+    // case types.LOGIN_REQUEST: {
+    //   console.log('Reducer: ', action.payload);
+    //   return state;
+    // }
+    case types.LOGIN_SUCCESS: {
+      const newState = { ...state };
+      newState.isLoggedIn = true;
+      newState.token = action.payload.token;
+      newState.user = action.payload.user;
+      return newState;
+    }
+    case types.LOGIN_FAILURE: {
+      const newState = { ...initialState };
+      return newState;
     }
     default: {
       return state;
